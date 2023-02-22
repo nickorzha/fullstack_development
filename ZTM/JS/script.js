81516dccbb09525240b1383692797edbcb7b6d50
fullstack_development <polaris891203@gmail.com>
2023-02-22 22:59:37 +0530

    Update script.js

--- ZTM/JS/script.js
+++ ZTM/JS/script.js
@@ -3,3 +3,49 @@ function multiply(a, b) {
 }
 
 console.log(multiply(2, 4))
+
+var list = ['tiger', 'cat', 'bear', 'bird']
+console.log(list[1])
+
+// Object
+var user = {
+  name: 'John',
+  age: 34,
+  hobby: 'Soccer',
+  isMarried: false,
+  spells: ['abrakadabra', 'shazam', 'boo'],
+  shout: function () {
+    console.log('AHHHHHHH!')
+  },
+}
+
+let list2 = [
+  function () {
+    console.log('hello')
+  },
+  function () {
+    console.log('goodbye')
+  },
+]
+
+var user2 = {
+  username: 'abhishek',
+  password: 'ztm@andrie',
+}
+
+var database = [user2]
+
+var newsFeed = [
+  {
+    username: 'Bobby',
+    timeline: 'So tired from all that learning!',
+  },
+  {
+    username: 'Sally',
+    timeline: 'Javascript is sooooo cool!',
+  },
+  {
+    username: 'Mitch',
+    timeline: 'Javascript is preeetyy cool!',
+  },
+]
